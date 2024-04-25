c49cd4c17e7838d96fbf0e3dd885f64a1c45efb4
AlexHrab <151546761+AlexHrab@users.noreply.github.com>
2024-04-26 01:27:03 +0300

    up

--- src/4.ts
+++ src/4.ts
@@ -1,3 +1,37 @@
+class Key {
+  private signature: number = Math.random();
+  public getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  constructor(private key: Key) {}
+  public getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  protected door: boolean = false;
+  protected tenants: Person[] = [];
+  constructor(protected key: Key) {}
+  public comeIn(tenant: Person) {
+    if (this.door === true) {
+      this.tenants.push(tenant);
+    }
+  }
+  public abstract openDoor(key: Key): void;
+}
+
+class MyHouse extends House {
+  public openDoor(key: Key): void {
+    if (this.key.getSignature() === key.getSignature()) {
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
 const house = new MyHouse(key);
@@ -6,6 +40,5 @@ const person = new Person(key);
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
